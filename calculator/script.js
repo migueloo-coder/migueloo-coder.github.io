53e4c6997a4fa84d8ce130ca9e74e8f1275163c4
migueloo-coder <migue.pinzon.caro22@gmail.com>
2024-03-20 11:24:14 +0000

    Update Project 20 MARCH 2024

--- calculator/script.js
+++ calculator/script.js
@@ -1,35 +1,164 @@
-// Asumiendo que tienes un HTML similar a la imagen con botones para cada número y operación.
+const dvKeyboard = document.querySelector("#divKey");
+const output = document.querySelector("#calculator");
+const btResultado = document.querySelector("#btequals");
+const btClear = document.querySelector("#clear");
+const btBackspace = document.querySelector("#btBackspace");
+const sinais = document.querySelectorAll(".sinais");
+const virgula = document.querySelector("#btdeci");
 
-// Seleccionamos todos los botones y la pantalla de la calculadora
-const buttons = document.querySelectorAll('button');
+
+let operacao;
+let primeiroValor;
+let segundoValor;
+let isSecondValue = false;
+
+
+//----------------------------------------------------------------------------------------------------------------
+
+
+dvKeyboard.addEventListener("click", (e) => {
+
+    const targetId = e.target.id;
+
+    if (e.target.classList.contains("button")) {
+        definirNumeros(targetId);
+        sinais.forEach((sinal) => { sinal.disabled = false; });
+        btBackspace.disabled = false;
+        btResultado.disabled = false;
+        btClear.innerText = "clear";
+
+    } else if (e.target.classList.contains("sinais")) {
+        operacao = definirSinal(e);
+
+    }
+})
+
+btResultado.addEventListener("click", (e) => {
+    const resultado = calcularResultado(e);
+    const resultFormatado = formatarNumero(resultado);
+    output.innerText = resultFormatado;
+    btBackspace.disabled = true;
+    primeiroValor = resultado;
+    isSecondValue = false;
+})
+
+btClear.addEventListener("click", () => {
+    operacao;
+    primeiroValor;
+    segundoValor;
+    isSecondValue = false;
+    output.innerText = 0;
+    sinais.forEach((sinal) => { sinal.disabled = true; });
+    btClear.innerText = "clear";
+    btResultado.disabled = true;
+})
+
+btBackspace.addEventListener("click", () => {
+    if (output.innerText.length > 1) {
+        output.innerText = output.innerText.slice(0, -1);
+        if (isSecondValue == false) {
+            primeiroValor = Number(output.innerText);
+        } else {
+            segundoValor = Number(output.innerText);
+        }
+    } else{
+        output.innerText = 0;
+        btClear.innerText = "clear";
+        sinais.forEach((sinal) => { sinal.disabled = true; });
+        btResultado.disabled = true;
+    }
+})
+
+virgula.addEventListener("click", () => {
+    output.innerHTML += ",";
+})
+
+
+
+//----------------------------------------------------------------------------------------------------------------
+
+
+const definirNumeros = (targetId) => {
+    if (output.innerText.length <= 11) {
+        if (output.innerText == 0) {
+            output.innerText = targetId.charAt(targetId.length - 1);
+        } else {
+            output.innerText += targetId.charAt(targetId.length - 1);
+        }
+
+        if (output.innerText.length >= 4) {
+            const num = Number(output.innerText.replace(/\./g, '').replace(/\,/g, '.'));
+            const numFormatado = formatarNumero(num);
+            output.innerText = numFormatado;
+          }
+
+        if (isSecondValue == false) {
+            primeiroValor = Number(output.innerText.replace(/\./g, '').replace(/\,/g, '.'));
+        } else {
+            segundoValor = Number(output.innerText.replace(/\./g, '').replace(/\,/g, '.'));
+        }
+    }
+}
+
+
+const definirSinal = (e) => {
+    sinais.forEach((sinal) => { sinal.style.backgroundColor = "#ff0084" })
+    e.target.style.backgroundColor = "#a10053";
+
+    const simbolo = e.target.innerText;
+
+    isSecondValue = true;
+    output.innerText = "0";
+
+    return simbolo
+}
+
+const calcularResultado = (e) => {
+    let result;
+    if (operacao == "÷") {
+        result = primeiroValor / segundoValor;
+    } else if (operacao == "×") {
+        result = primeiroValor * segundoValor;
+    } else if (operacao == "+") {
+        result = primeiroValor + segundoValor;
+    } else {
+        result = primeiroValor - segundoValor;
+    }
+
+    sinais.forEach((sinal) => { sinal.style.backgroundColor = "#ff0084" })
+
+    return result;
+}
+
+const formatarNumero = (num) => {
+    const numFormatado = num.toLocaleString('pt-BR');
+    return numFormatado;
+  };
+
+
+/*const buttons = document.querySelectorAll('button');
 const display = document.querySelector('.display');
 
-// Añadimos un evento de clic a cada botón
 buttons.forEach(function(button) {
   button.addEventListener('click', calculate);
 });
 
-// Función de cálculo
+
 function calculate(event) {
-  // Obtenemos el valor del botón presionado
+  
   const clickedButtonValue = event.target.value;
 
-  if (clickedButtonValue === '=') {
-    // Si el botón es igual a '=', calculamos y mostramos el resultado
-    if (display.value !== '') {
-      // Intentamos calcular y atrapar cualquier error que ocurra en el cálculo
-      try {
-        // Evaluamos la expresión y mostramos el resultado
+  if (clickedButtonValue === 'equals') {
+      if (display.value !== '') {
+      try {    
         display.value = eval(display.value);
       } catch (error) {
         display.value = 'Error';
       }
     }
-  } else if (clickedButtonValue === 'AC') {
-    // Si el botón es 'AC', borramos la pantalla
+  } else if (clickedButtonValue === 'clear') {
     display.value = '';
   } else {
-    // Para cualquier otro botón, agregamos el valor del botón a la pantalla
     display.value += clickedButtonValue;
   }
-}
+}*/
